cbd5ac6b629b6702abd1b7c5f9490ad4ca6f2480
cai-dong-cheng <1940099192@qq.com>
2020-11-02 20:01:13 +0800

    day14

---
+++ day14/RegexMatches.java
@@ -0,0 +1,32 @@
+package com.homework.day14;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author CDC
+ * 从一个给定的字符串中找到数字串
+ */
+public class RegexMatches
+{
+    public static void main( String args[] ){
+
+        // 按指定模式在字符串查找
+        String line = "This order was placed for QT3000! OK?";
+        String pattern = "(\\D*)(\\d+)(.*)";
+
+        // 创建 Pattern 对象
+        Pattern r = Pattern.compile(pattern);
+
+        // 现在创建 matcher 对象
+        Matcher m = r.matcher(line);
+        if (m.find( )) {
+            System.out.println("Found value: " + m.group(0) );
+            System.out.println("Found value: " + m.group(1) );
+            System.out.println("Found value: " + m.group(2) );
+            System.out.println("Found value: " + m.group(3) );
+        } else {
+            System.out.println("NO MATCH");
+        }
+    }
+}
